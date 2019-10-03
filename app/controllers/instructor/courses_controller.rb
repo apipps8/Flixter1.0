e6dbe76f2a30712d37633f561950aa75ca4312f6
Amanda Pippins <apipps8@gmail.com>
2019-10-03 00:03:58 +0000

    Listing the Courses

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -1,13 +1,17 @@
 class Instructor::CoursesController < ApplicationController
-	before_action :authenticate_user!
-	
-	def new
-		@course = Course.new
-	end
+  before_action :authenticate_user!
 
-	def create
+  def new
+    @course = Course.all
+  end
+
+  def create
     @course = current_user.courses.create(course_params)
-    redirect_to instructor_course_path(@course)
+    if @course.valid?
+      redirect_to instructor_course_path(@course)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -19,6 +23,4 @@ class Instructor::CoursesController < ApplicationController
   def course_params
     params.require(:course).permit(:title, :description, :cost)
   end
-
 end
- 

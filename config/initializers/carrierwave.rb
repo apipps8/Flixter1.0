b22527a34246c6346d81ab266fd30014b137c506
Amanda Pippins <apipps8@gmail.com>
2019-10-21 16:12:06 +0000

    Adjusting the Image Resolution update

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,13 @@
 # config/initializers/carrierwave.rb
 
 CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'                        # required
-  config.fog_public = false
-  config.fog_credentials = {
-    provider:              'AWS',                        # required
-    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
-    aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
+  config.storage    = :aws
+  config.aws_bucket = ENV["AWS_BUCKET"]
+  config.aws_acl    = "public-read"
+
+  config.aws_credentials = {
+      access_key_id:     ENV["AKIAYL7LSOU3DBDSPXUA"],
+      secret_access_key: ENV["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"],
+      region:            ENV["us-east-1"]
   }
-  config.fog_directory  = ENV["AWS_BUCKET"]              # required
 end

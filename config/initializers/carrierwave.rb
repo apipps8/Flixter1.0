7c46db52fb6a9a4ef4b09727355aa58686bb0455
Amanda Pippins <apipps8@gmail.com>
2019-10-25 02:04:52 +0000

    Adjusting the Image Resolution update2

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,15 @@
 # config/initializers/carrierwave.rb
 
 CarrierWave.configure do |config|
-  config.storage    = :aws
+ config.storage    = :aws
   config.aws_bucket = ENV["AWS_BUCKET"]
   config.aws_acl    = "public-read"
 
   config.aws_credentials = {
-      access_key_id:     ENV["AKIAYL7LSOU3DBDSPXUA"],
-      secret_access_key: ENV["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"],
-      region:            ENV["us-east-1"]
+      access_key_id:     ENV["AWS_ACCESS_KEY"],
+      secret_access_key: ENV["AWS_SECRET_KEY"],
+      region:            ENV["AWS_REGION"]
   }
+
 end
+ 

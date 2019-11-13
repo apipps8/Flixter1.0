ebfbbacd775f97ea9948d7af0fe2770c39ed4b01
Amanda Pippins <apipps8@gmail.com>
2019-11-13 05:40:52 +0000

    adding drag and drop update3

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -10,7 +10,7 @@ class Instructor::LessonsController < ApplicationController
   def create
     @section = Section.find(params[:section_id])
     
-    @lesson = @section.lesson.create(lesson_params)
+    @lesson = @section.lessons.create(lesson_params)
     redirect_to instructor_course_path(@section.course)
   end
 
@@ -27,7 +27,17 @@ class Instructor::LessonsController < ApplicationController
     end
   end
 
+  def require_authorized_for_current_section
+    if current_section.course.user != current_user
+      render plain: 'Unauthorized', status: :unauthorized
+    end
 
+  end
+
+ helper_method :current_section
+  def current_section
+    @current_section ||= Section.find(params[:section_id])
+  end
 
   def lesson_params
 params.require(:lesson).permit(:title, :subtitle, :video, :row_order_position)
